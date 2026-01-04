5dea8cba4fe88e703b8030a33e2e2e5c4f739b6a
Joel <itsmejoel222@gmail.com>
2026-01-04 15:23:53 +0530

    problems sorted abt pdf

--- src/pages/training/Service.js
+++ src/pages/training/Service.js
@@ -81,7 +81,7 @@ function Service() {
   useEffect(() => {
     const fetchClients = async () => {
       try {
-        const res = await fetch(`${API_BASE}/admin/service`);
+        const res = await fetch(`${API_BASE}/api/clients`);
         const data = await res.json();
         setClients(data || []);
       } catch (err) {
